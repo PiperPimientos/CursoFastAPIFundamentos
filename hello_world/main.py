5d5b20ed105bda28d99ca70d78f5ec1b0d26915f
felipe <feliperestrepomeneses@gmail.com>
2021-11-01 18:12:50 -0500

    hicimos otro branch, llamado validations_query,en donde aprendemos como funcionan las validaciones en los query parameters

--- hello_world/main.py
+++ hello_world/main.py
@@ -1,59 +1,37 @@
-# El Request Body. Para entenderlo tenemos que retomar un concepto del curso de intro al Backend. Ya sabemos lo que es un HTTP, que tiene, headers, body, métodos.
-# Luego pensemos que tenemos un cliente, que es nuestro pc y tenemos un servidor. Sabemos que esta comunicación se realiza mediante HTTP
- 
-# Cuando es el cliente que se comunica con el servidor, esta comunicación se denomina Request body, y cuando el servidor toma el request del cliente y responde, tenemos un response body. Ambos body se envían mediante un formato JSON.
-# Ahora, para  lo siguiente que haremos es crear otro git Branch, utilizaremos el nombre git checkout -b “request_response_body”.
-# Una vez que hacemos este nuevo Branch, empezaremos nuestro proyecto de esta clase:
+#Validaciones: Query Parameterss
 
-# #FastAPI
-# from fastapi import FastAPI
+# En algunas ocasiones necesitaremos que un query parameter o que un path parameter tenga una forma especifica.
+# Por ejemplo tener un query parameter que se refiere al nombre de una persona y este nombre, no debería tener mas de 50 caracteres, de hecho seria muy extraño que exista un nombre de mas de 50 caracteres. Si permitimos que sucedan estas cosas, alguien podría introducir un texto mas grande que la biblia y terminar rompiendo nuestra base de datos.
+# Vamos a ver como validar este tipo de parámetros
+# Supongamos que tenemos el siguiente path
+# http://myapp.com/API/Person/detail
 
-# app = FastAPI()
+# Y supongamos que este endpoint nos trae el detalle de una persona que se registro en la aplicación. 
+# Supongamos que person tiene Name, Age, Hair_color, etc. Y supongamos que la persona necesite información sobre el nombre y la edad. Pues podría hacerlo mediante Query Parameters
 
-# @app.get("/")
-# def home():
-#     return {"Hello": "World"}
+# http://myapp.com/API/Person/detail?name=miguel&age=23 
 
-#Tenemos importado nuestro FastAPI, además tenemos una variable app que tiene instanciada la clase FastAPI() y, mas abajo, tenemos un path operation, conformado por el path operation decorator, donde estamos utilizando el método http .get, en el endpoint “/”, y para ello utilizaremos una función que retornara un archivo JSON, con la llave hello y el valor world.
+# Ahora, que pasa si por alguna razón, que en el nombre nos pone un texto infinito…
+# Vamos a tener que decirle al usuario que ese name no puede ser mayor a 50 caracteres y mayor o igual a 1.
+# Vamos a crear un nuevo git Branch para esta clase, llamada validations_query. Esto lo hacemos con git checkout -b “validations_query”
+# 1.	Ahora empezaremos con un path decorator que será de método .get que ira dirigido al path /person/detail.
+# Despues una path operation function que denominaremos show_person()
+# Dentro de los parámetros de la funcion (query parameters), vamos a elaborar los dos, tanto de person, como de age. Pero estos no los tengo que poner en el endpoint del path decorator, los ponemos en los parámetros de la definición de la funcion.
+# Diremos que name es un optional porque es un query parameter y recordemos que los query parameters son opcionales. Igual con age. Pero les diremos que es un parámetro de tipo = Query. Para ello tendremos que importar la clase Query de fastapi.
+# Con Query tenemos mas o menos los mismos parámetros que en Body, pero tienen algunas diferencias.
+# Lo primero que haremos es poner que el default de Query, es None, pero si la persona llega a mandar cierta información, colocamos el min_length=1, es decir que la persona no puede enviar un nombre menor a 1 string y el max_length=50
+# Para age, lo mismo, diciendo que es un str y diciendo que es obligatorio, ósea que como  parámetro el Query no recibe None, sino los tres puntos, …, esto no es ideal, pues lo ideal es que un Query siempre sea opcional, pero podría pasar que necesitemos que sea obligatorio. Ahora bien lo que si es buena práctica, es que siempre que un parámetro tenga que ser obligatorio, debería ser un Path Parameter no un Query Parameter
 
+# @app.get(“/person/detail”)
+# def show_person(
+#                    name: Optional[str] = Query(None, min_Length=1,                      max_Length=50),
+#                    age: str = Query(…, 
+# ):
+# 2.	Ahora cerramos los parametros de la funcion y lo cerraremos con un return del json que tendrá como llave a el str name y un valor que será el age.
+# return{name: age}
 
-#Ahora practicaremos el Request and Response Body
-# 1.	Creamos una nueva path operation, en donde tendremos una petición de tipo .post (metodo), porque vamos a enviar datos desde el cliente al servidor y si tuviéramos que recibir desde el servidor hasta el cliente, utilizando el método .get. Y finalmente se nos ocurre que vamos a tener un endpoint llamado /person/new, y si yo accedo a este endpoint, sere capaz de crear una nueva person. Porque pensemos que esta API tendrá la funcionalidad de crear personas nuevas con sus datos.
-# Despues definiremos el path operation function, es decir crearemos la función para crear nueva persona.
-
-# @app.post(“/person/new”)
-# def create_person():
-#          pass
-
-# Y hasta aquí lo dejaremos, porque para aprender request body, tendremos que aprender un concepto mas
-#Este concepto es Models
-# Para saber que es un Modelo, tenemos que imaginarnos a una entidad. Una entidad puede ser cualquier cosa en el mundo, como una persona, una casa, un animal, etc. Un modelo, no es más que la representación de una entidad en código, al menos de manera descriptiva.
-# Como luce un modelo en FastAPI?
-# Pensemos que por ejemplo para la API de twitter tenemos un modelo que es el Tweet y el otro modelo que es el User. Es decir un modelo que describa a la entidad Tweet y otro modelo que describa a la entidad User.
-# A estos modelos los vamos a crear con una librería llamada Pydantic, de la cual nos traeremos una clase llamada Base Model, que nos va permitir crear los modelos
-# En nuestro código
-# 1.	Pondremos por encima de FastAPI a Pydantic, porque Pydantic esta en la base o por debajo de FastAPI que funciona sobre Pydantic. Por ello es necesario que vayamos importando con este nivel de abstracción. Y de base de Pydantic podríamos importar incluso librerías nativas de Python, que para este caso necesitaremos de la librería typing, importar un tipo de dato para realizar tipado estatico que será Optional.
-# from typing import Optional
-# from pydantic import BaseModel
-# 2.	Una vez importado BaseModel, crearemos los modelos necesarios para la aplicación. Esto lo haremos por el momento debajo de la definicion de la variable app
-
-# Primero hay que crear una class, luego el nombre del modelo que será Person, tiene que heredar de (BaseModel):
-# Debajo empezamos a definir el modelo, colocando los atributos de la entidad.
-# Primer atributo sera el first name, que será de tipo str (tipado estatico) y lo mismo con los demas
-# first_name: str
-# last_name: str
-# age: int
-# hair_color: str
-# is_married: bool
-# Pero suponiendo que el hair color  y el is married no es importante, lo dejaremos como opcionales, para eso fue que importamos Optional, de la librería typing.
-# Para eso les quitamos el tipo str y bool. Y seguido colocaremos Optional y entre corchetes [ ] vamos a colocar el tipo valor que nosotros esperaríamos, es decir str y bool. 
-# Pero además tendremos que tener un valor por defecto por si el usuario no nos introduce los datos que son opcionales. Normalmente, si estamos trabajando en bases de datos, el valor va ser = null. Null en Python esta representado como None, es decir que aquí puede haber algo o no puede haber nada.
-# 3.	Ahora iremos al path operation que habíamos creado para persona nueva y en la función podemos retornar directamente person(path) y la cosa va funcionar. Sin embargo necesitamos recibir de la persona el modelo de Person. Como lo recibimos? Con el Request Body, que también es un parámetro que se coloca en la definicion de la función, además tendremos que utilizar tipado estatico, es decir que tendremos a ese person(el path) que será de tipo Person(la clase)
-# def créate_person(person: Person):
-# 4.	Sin embargo, hay algo mas. De fastapi vamos a importar otra clase llamada Body. Esta clase nos permite decir explícitamente que un parámetro que me esta llegando es de tipo body() y, como parámetro le pasamos el triple punto, este parámetro significa que este Request Body, es obligatorio. Es decir que siempre que encontremos los tres puntos, va significar que un atributo o parámetro es obligatorio.
-# def create_person(person: Person = Body(…))
-# Ya con esto estamos listos para retornar person
-
+# Ahora, si vamos a nuestra documentación intereactiva veremos que tendremos otra path operations que será de método .get, que se llama Show Person, y se accede mediante el endpoint /person/detail. 
+# Adentro veremos que tenemos un Query Parameter, que tendrá name, con una validación de max length y min length y luego el age que será un required por su obligatoriedad. 
 
 
 #Python
@@ -65,6 +43,7 @@ from pydantic import BaseModel
 #FastAPI
 from fastapi import FastAPI
 from fastapi import Body
+from fastapi import Query
 
 app = FastAPI()
 
@@ -80,9 +59,19 @@ class Person(BaseModel):
 def home():
     return {"Hello": "World"}
 
+#Request and Response body
+
 @app.post("/person/new")
 def create_person(person: Person = Body(...)):
     return person
 
+# Validaciones: Query Parameters
 
-#Para ver como funciona en la documentacion interactiva, ver las notas
+@app.get("/person/detail")
+def show_person( 
+    name: Optional[str] = Query(None, min_Length=1, max_Length=50),
+    age: str = Query(...)
+):
+
+    return {name: age}
+
