b4c32f3e587ae0b9656ed2569ea1d819656ae537
felipe <feliperestrepomeneses@gmail.com>
2021-11-01 22:25:36 -0500

    agregamos el branch validations_models para ver como validar los modelos que hemos creado en cada uno de sus atributos y no solo mandar el request body, validando que es un request body

--- hello_world/main.py
+++ hello_world/main.py
@@ -1,44 +1,32 @@
-#Validaciones: Path Parameters
+#Validaciones: Models
 
-# Ahora validaremos el request body.
-# 1.	Crearemos la nueva path operation que va utilizar una operation de tipo put, que sirve para actualizar algún contenido en nuestra aplicación.
-# 2.	Va tener una path operation que será /person/{person_id}. Es decir que cada vez que se use este path operation se podrá actualizar un contenido, que en este caso será person_id
-# 3.	Para que esto funcione, tendremos que hacer un request body. Empezando con la path operation function, que tendrá de nombre update_person(). Dentro de esta tebdrenis obviamente que definir el person id: que será tipo int y que será igual a Path y todos los parámetros que teníamos en person_id.
-# 4.	Ahora haremos el request body, a este request body que la persona me envia, yo le tengo que poner un nombre y luego llamar la clase Person que habíamos creado al principio,
-# #Models
-# class Person(BaseModel):
-#     first_name: str
-#     last_name: str
-#     age: int
-#     hair_color: Optional[str] = None
-#     is_married: Optional[bool] = None
+# Vimos como combinar dos request bodys en una misma path operation, pero tenemos que saber validar perfectamente los atributos de un request body.
+# Esto estará en el nuevo git Branch, “validations_models”, porque vamos a aprender a validar los modelos.
+# 1.	Cuando nosotros vamos a validar un request body tenemos que hacerlo directamente en la definición del modelo(clase) que nosotros utilizamos para ese request body. 
+# 2.	Vamos a validar todos los atributos que tiene Person. Para ello necesitaremos una nueva clase pydantic, que será Field. Field es exactamente igual a Body, Query y Path pero esta directamente relacionada a los modelos de pydantic.
+# 3.	En nuestro Modelo colocaremos al final de todos los atributos un igual seguido del nombre de la clase pydantic, Field
+# Por ejemplo:
+# first_name: str = Field(…, min_Length=)
+# Y el constructor lleva exactamente los mismos atributos de la clase Query y path, es decir que será obligatorio, luego que tendremos un min_length y luego un max_length, la mismas validaciones. Ahora age, tendrá una validación distinta, que serán: obligatorio y que la edad tiene que ser greater than (gt), 0. y menor o igual que 115
+# 4.	Para hair color y para is married, sabemos que son opcionales. 
+# Para el caso particular de is married utilizaremos la sintaxis de Field, al cual le tendríamos que agregar como parámetro un default=None
+# Sin embargo para hair color, sabemos que es un string que debe ser validada, pero hay muchos tipos de hair color, y para ello utilizaremos una nueva clase que viene directamente de Python. Tendremos que llamar el modulo enum para importar la clase Enum
+# Enum nos sirve para crear enumeraciones de strings, asi poder definir las validaciones del atributo de hair color.
+# Dentro de models vamos a tener que crear una nueva clase llamada HairColor(Enum), que contendrá esas posibles validaciones
+# “white” = “white”
+# “brown” = “brown”
+# Y asi con los demás colores de pelo,
+# 5.	Luego en el Model de person, sin bien hair color es Optional, lo cambiaremos por un Field y este Field va tener por default, None. Y el tipo ya no será str sino HairColor, esto nos asegura que esas validaciones sean del tipo que definimos en el Model HairColor
 
-
-# Es decir que la persona que nos este enviando el update debería enviarnos un JSON con ese tipo de datos.
-# 5.	Ahora finalmente que dicha clase es igual a una clase de fastapi que ya conocemos, = Body(…) que además, sera obligatorio.
-# 6.	Ahora, dentro de la funcion, solo nos queda colocar el return, que en este caso será person
-# 7.	Si ahora nos vamos a la documentación interactiva, notaremos que tenemos una nueva path operation de método .put, de nombre Update_person, que esta en el path de /person/{person_id}.
-# Si lo ejecutamos vemos que nos pedirá el person_id y luego vendrá el request body que viene con un JSON que tendremos que cambiar, porque es además un required.
-
-# Ahora pensemos, que pasa si en este mismo path operation le pedimos al usuario en un request body, el parámetro Location.
-# 1.	El request body de nombre Location de tipo Location  será igual a una clase fastapi Body(…) que será obligatorio. Con lo que le estaremos diciendo al usuario en una sola path operation que nos envie no solo 1 sino 2 JSONs, esto se supone que no se puede lograr, pero veremos como lo soluciona FastAPI.
-# 2.	Arriba en nuestros Models, crearemos el Modelo Location(class Location), recordando que tendremos que llamar a nuestra clase de pydantic BaseModel, 
-# A esa clase le daremos los siguientes atributos:
-# a. City: str
-# b.	State: str
-# c.	Country: str
-# 3.	Finalmente volvemos a nuestro request body y la manera en la que podremos retornar esta clase es hacerlo de forma explicita, no podemos dejar a FastAPI que lo haga por si solo. 
-# Dentro de la path operation function, tenemos que agregar antes del return, la variable results que tendrá asignado el request body person, convertido a diccionario, ósea con un .dict()
-# Ahora, en una nueva línea invocamos la variable results con el método .update(location()) y con el Body location también convertido a diccionario. Lo que estamos haciendo es convertir el diccionario person y el diccionario location
-# Y finalmente cambiaremos el return que estaba con person, por results.
-
+# Si ahora nos vamos a la documentación interactiva. Y si vamos a ver nuestro request body, veremos que swagger automáticamente nos muestra la nueva estructura, si por ejemplo vemos que hair_color por defecto aparece white:
 
 
 #Python
 from typing import Optional
+from enum import Enum
 
 #Pydantic
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 #FastAPI
 from fastapi import FastAPI
@@ -49,6 +37,13 @@ app = FastAPI()
 
 #Models
 
+class HairColor(Enum):
+    white = "white"
+    brown = "brown"
+    black = "black"
+    blonde = "blonde"
+    red = "red"
+
 #Location Model
 class Location(BaseModel):
     city: str
@@ -57,11 +52,23 @@ class Location(BaseModel):
 
 #Person Model
 class Person(BaseModel):
-    first_name: str
-    last_name: str
-    age: int
-    hair_color: Optional[str] = None
-    is_married: Optional[bool] = None
+    first_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50
+    )
+    last_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50
+    )
+    age: int = Field(
+        ...,
+        gt=0,
+        Le=115
+    )
+    hair_color: Optional[HairColor] = Field(default=None)
+    is_married: Optional[bool] = Field(default=None)
 
 @app.get("/")
 def home():
@@ -121,10 +128,3 @@ def update_person(
     results = person.dict()
     results.update(location.dict())
     return results
-
-# Aunque también hay una manera mas resumida de hacerlo, con la unión de diccionarios que recordemos seria con la siguiente sintaxis
-# person.dict() & location.dict()
-# Sin embargo, FastAPI todavia no soporta esta sintaxis, por lo que toca hacerlo de manera mas clásica. Conviritiendo primero a diccionario el primer parámetro que tenemos y luego con el método .update(location.dict()) de esta variable results, unir como diccionario al segundo parámetro.
-
-# Si hacemos los cambios y luego le damos execute, y al final vamos a ver el Response body, encontraremos un solo formato JSON con todas las respuestas del request body, pero sin diferenciar de los atributos de la clase person y los atributos de la clase location.
-
