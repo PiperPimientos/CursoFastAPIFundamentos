31064dc2a06cb98e6af9dd4b7be159ccbcdf6e6c
felipe <feliperestrepomeneses@gmail.com>
2021-11-04 08:54:55 -0500

    agreamos el branch cookies_and_headers_parameters para ver los otros tipos de path operations que podemos hacer con estas clases Header y Cookie de fastapi

--- hello_world/main.py
+++ hello_world/main.py
@@ -1,34 +1,39 @@
-#Status Code
+#Cookie and Header parameters
 
-# El status code es ese pequeño numero de 3 digitos que le indica al cliente que paso con la respuesta de esa petición HTTP.
-# Basicamente lo que hace la API es hacer el response que contiene casi siempre un JSON, deberíamos tener distintos status code que nos indiquen como salieron las cosas.
-# Dentro de una centena de status code, podemos encontrar otros casos especiales:
-# 201: Algo se creo (created), por ejemplo si estamos en una path operation desde el cliente al servidor, que envia una petición de tipo post y en esa petición creamos un usuario y guardamos el usuario en la base de datos, el servidor responde 201.
-# 204: No content. Es decir que no hay ninguna respuesta, aunque todo haya salido bien.
-# 404: Accedimos a un endpoint que no existe. (No exists)
-# 422: Validation error. Es decir que el cliente nos envia un dato que no esta en el formato que esperábamos. Por ejemplo si esperábamos un validation de no mas de 20 caracteres y le llega al server de 21 caracteres.
-#  Ahora haremos un git checkout -b para nuestra nueva Branch, que se llamara status_code. 
-# Lo que haremos es que cada una de las path operations tenga un status code personalizado. 
-# 1.	Importamos el modulo de fastapi, status. Que nos permite acceder a los diferentes status code de http para poder ingresarlos en nuestras path operations.
-# 2.	Los status code se colocan en el path operation decorator, justo despues del endpoint
-# la sintaxis seria asi: @app.get(“/”, status_code=status.HTTP_200_OK)
-# 3.	Para “/” utilizaremos el 200
-# Aquí un truco: si al endpoint le agregamos path=, quedara mucho mas ordenado en las líneas de código
-# 4.	Para /person/new vamos a ponerle 201 CREATED, porque este endpoint es para crear una persona en nuestra base de datos
-# 5.	Para el de  /person/detail vamos a ponerle 200
-# 6.	RETO: ponerle el status code al resto de Path operations
+# Ya aprendimos como ingresar datos a la API mediante path parameters, query parameters, request bodys y formularios. Hay fuentes de datos todavía mas exóticas. Son dos cookies y headers parameters.
+# Por ejemplos header ya los hemos visto, es una parte de una petición o respuesta http que contiene datos sobre ella, como por ejemplo la fecha en que se hizo, en que formato viene, siendo esto último por ejemplo que vimos que swagger UI nos muestra si la application era un JSON o era un formulario. Tambien existen otros, como el user agent que nos dice quien esta entrando a nuestra web y quien esta usando nuestra API. 
+# Una cookie es una pieza de código pequeña que un servidor mete en el PC cuando estamos navegando en el PC, esos datos van a ser útiles para facilitar la navegación en una pagina web despues de la primera vez.
+# Las clases para trabajar con cookies y headers en FastAPI, son esas mismas, cookies y headers.
+# Crearemos un git checkout -b “cookie_and_headers_parameters”.
+# 1.	Vamos a crear los Cookies and Headers Parameters. Vamos a crear toda la path operation.
+# 2.	El path operation decorator será de tipo .post porque vamos a trabajar con un envio de información desde el frontend hacia el servidor.
+# El path será /contact, pues vamos a crear un formulario de contacto de la pagina web.
+# El status code de esta response será 200
+# 3.	Ahora haremos nuestro path operation function.
+# En sus parámetros necesitaremos el first_name, que es el nombre de la persona, y como viene de un formulario será entonces un Form. Dentro de los parámetros de este form tendremos que es obligatorio . . ., max_Length=20, min_length=1.
+# Haremos el otro parámetro que será last_name, con las mismas propiedades del anterior.
+# Vamos a pedir también el email, para ello utilizaremos ese tipo de dato exotico de pydantic que es EmailStr y que igual será un Form, que a su vez tendra parámetro de ser obligatorio.
+# Luego Sigue el Message que será un str y también va ser un Form, que será obligatorio, tendra un min_length=20. Este será el mensaje que nos envie el usuario
+# Ahora agregaremos el primer header, que será el user_agent, que nos dira quien esta intentando usar a esta parte de la API. Por el momento colocaremos que será Optional[str] y que será un = Header. Para ello tendremos que importar esta clase de fastapi y de paso también vamos a importar Cookie. El valor por default de Header va ser None, porque a veces puede venir y a veces no.
+# Por ultimo tendremos un parámetro llamado ads que va a controlar las cookies que nos envia el servidor que esta trabajando con la API. el ads será de tipo Optional[str] y será una = Cookie, con parámetro default None.
+# Ahora, dentro de la funcion, simplemente retornaremos user_agent
+# Nota: Recordemos que ppara utilizar el EmailStr tenemos que tener instalado el pydantic email en la terminal con “pip install pydantic[email]”
+# 4.	Si vamos a nuestra documentación interactiva encontraremos que tenemos un nuevo path operation que será de tipo post y estará en /contact. Tendra dos parámetros que ingresar, que corresponderán al de header y cookie. En user_agent no le pondremos nada, lo capturara por defecto. En ads colocaremos “This is the info that is tracking you”.
+# Luego podremos ver el formulario con el first name, last name, email, message, en message por ejemplo “I am Felipe and Im trying to learn FastAPI in python”.
+# Si vemos el response body veremos que nos retorna, tal y como dijimos en el código, el user_agent. Nos aparece el valor de la cabecera http user_agent de la petición post de la PC de la persona que esta usando la API. Nos dice por ejemplo si estamos en Windows.
 
 
 #Python
 from typing import Optional
 from enum import Enum
+from fastapi.datastructures import Default
 
 #Pydantic
 from pydantic import BaseModel, Field, EmailStr
 
 #FastAPI
 from fastapi import FastAPI
-from fastapi import Body, Query, Path, status
+from fastapi import Body, Query, Path, status, Form, Header, Cookie
 
 
 
@@ -118,6 +123,14 @@ class Person(PersonBase):
 class PersonOut(PersonBase):
     pass
 
+#LoginOut Model
+class LoginOut(BaseModel):
+    username: str = Field(
+        ..., 
+        max_length=20, 
+        example="Miguel2021"
+        )
+    message: str = Field(default="Login Succesfully!")
 
 #Path Operations
 
@@ -200,3 +213,40 @@ def update_person(
     # results.update(location.dict())
     # return results
     return person
+
+#Form de login
+
+@app.post(
+    path="/login",
+    response_model=LoginOut,
+    status_code=status.HTTP_200_OK
+)
+def login(username: str = Form(...), password: str = Form(...)):
+    return LoginOut(username=username)
+
+#Cookies and Headers Parameters
+
+@app.post(
+    path="/contact",
+    status_code=status.HTTP_200_OK,
+)
+def contact(
+    first_name: str = Form(
+        ...,
+        max_Length=20,
+        min_Length=1
+    ),
+    last_name: str = Form(
+        ...,
+        max_Length=20,
+        min_Length=1
+    ),
+    email: EmailStr = Form(...),
+    message: str = Form(
+        ...,
+        min_Length=20
+    ),
+    user_agent: Optional[str] = Header(default=None),
+    ads: Optional[str] = Cookie(default=None)
+):
+    return user_agent
